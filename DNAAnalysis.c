42cd0fbc8648582052adb5db959355eb9e904047
Leah Torres <leahtorres15@gmail.com>
2017-09-23 22:17:45 -0700

    Added quit functionality, first release

--- DNAAnalysis.c
+++ DNAAnalysis.c
@@ -17,51 +17,31 @@ int main (int argc, char *argv[])
 
    while (1)
    {
-      //printf("%sChoice: ", menu);
       fgets(line, sizeof(line), stdin);
-/* ******Get Rid of Choice Stuff*********
-      isint = sscanf(line, "%d", &choice);      
-      if(! isint)
+      NucleotideNumber = strlen(OriginalDNAStrand);
+      char *RNAStrand = (char*) malloc(NucleotideNumber*sizeof(char));
+      if(OriginalDNAStrand[0] == 'Q' || OriginalDNAStrand[0] == 'q')
+          {
+             return 1;
+          }
+      for(i=0; i<NucleotideNumber; i++)
       {
-         printf("Invalid Choice.\n");
-         continue;
-      }*/
-
-//      if(choice == 1)
-//      {
-         NucleotideNumber = strlen(OriginalDNAStrand);
-         char *RNAStrand = (char*) malloc(NucleotideNumber*sizeof(char));
-         for(i=0; i<NucleotideNumber; i++)
-         {
-            RNAStrand[i] = ConverterRNA(OriginalDNAStrand[i]);
-            if(RNAStrand[i] == 0)
-            {
-               printf("Enter proper DNA code.\n");
-               return 1;
-            }
-         } 
-         printf("Original DNA Strand: %s\n", OriginalDNAStrand);
-         printf("RNA: %s\n", RNAStrand);
-//         printf("Number of Nucleotides Analyzed: %d\n\n", NucleotideNumber);
-//      }
-//      else if(choice == 2)
-//      {
-//         printf("Input DNA Strand: ");
-//         scanf("%s", &OriginalDNAStrand);
-//         NucleotideNumber = strlen(OriginalDNAStrand);
-         char *NewDNAStrand = (char*) malloc(NucleotideNumber*sizeof(char));
-         for(i=0; i<NucleotideNumber; i++)
-         {
-            NewDNAStrand[i] = ConverterDNAComplement(OriginalDNAStrand[i]);
-            if(NewDNAStrand[i] == 0)
-            {
-               printf("Enter proper DNA code.\n");
-               return 1;
-            }
-         }
-         printf("Complementary DNA Strand: %s\n", NewDNAStrand);
-         printf("Number of Nucleotides Analyzed: %d\n\n", NucleotideNumber);
-//      }
+          RNAStrand[i] = ConverterRNA(OriginalDNAStrand[i]);
+	  if(RNAStrand[i] == 0)
+          {
+              printf("Enter proper DNA code.\n");
+              return 1;
+          }
+       } 
+       printf("Original DNA Strand: %s\n", OriginalDNAStrand);
+       printf("Complementary RNA Strand: %s\n", RNAStrand);
+       char *NewDNAStrand = (char*) malloc(NucleotideNumber*sizeof(char));
+       for(i=0; i<NucleotideNumber; i++)
+       {
+          NewDNAStrand[i] = ConverterDNAComplement(OriginalDNAStrand[i]);
+       }
+       printf("Complementary DNA Strand: %s\n", NewDNAStrand);
+       printf("Number of Nucleotides Analyzed: %d\n\n", NucleotideNumber);
 
       printf("Input DNA Strand: ");
       scanf("%s", OriginalDNAStrand);
